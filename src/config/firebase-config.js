0e121e2c4cfb09ec8258c40af529d4eac3ae3d34
Arman <125900320+Syfer0@users.noreply.github.com>
2023-12-18 11:10:22 +0530

    first commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLAEutAHHdugqyDOr9OAFjnYTCChCkw_c",
+  authDomain: "portfolio-arman-mulani.firebaseapp.com",
+  projectId: "portfolio-arman-mulani",
+  storageBucket: "portfolio-arman-mulani.appspot.com",
+  messagingSenderId: "411146772526",
+  appId: "1:411146772526:web:22ccb0a7c1ed3bbd5bffbf",
+  measurementId: "G-89SGBXN1PY",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
